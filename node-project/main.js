e35eb6d713ba39f7cee71c86238c0e217f1556ec
Ricardogn224 <abrahamricardogn224@gmail.com>
2023-06-19 13:04:08 +0200

    add storage systeme with json files

--- node-project/main.js
+++ node-project/main.js
@@ -1,19 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
-//liste des utilisateurs
-const users = [
-  { id: 1, name: 'John Doe', accounts: [1, 2] },
-];
+const usersFilePath = './users.json';
+const accountsFilePath = './accounts.json';
 
-//liste des comptes
-const accounts = [
-  { id: 1, userId: 1, balance: 100 },
-  { id: 2, userId: 1, balance: 200 },
-];
+let users = [];
+let accounts = [];
+
+// Load users and accounts data from JSON files
+function loadUserData() {
+  try {
+    const usersData = fs.readFileSync(usersFilePath, 'utf8');
+    users = JSON.parse(usersData);
+  } catch (error) {
+    // If the file doesn't exist, create an empty array
+    users = [];
+    saveUserData(); // Save empty user data to JSON file
+  }
+}
+
+function loadAccountData() {
+  try {
+    const accountsData = fs.readFileSync(accountsFilePath, 'utf8');
+    accounts = JSON.parse(accountsData);
+  } catch (error) {
+    // If the file doesn't exist, create an empty array
+    accounts = [];
+    saveAccountData(); // Save empty account data to JSON file
+  }
+}
+
+// Save users and accounts data to JSON files
+function saveUserData() {
+  const usersData = JSON.stringify(users, null, 2);
+  fs.writeFileSync(usersFilePath, usersData, 'utf8');
+}
+
+function saveAccountData() {
+  const accountsData = JSON.stringify(accounts, null, 2);
+  fs.writeFileSync(accountsFilePath, accountsData, 'utf8');
+}
+
+// Load initial user and account data
+loadUserData();
+loadAccountData();
 
 // Route : /users/:id/accounts/debit
 app.post('/users/:id/accounts/debit', (req, res) => {
@@ -22,7 +56,6 @@ app.post('/users/:id/accounts/debit', (req, res) => {
 
   // Verifier que l'utilisateur existe
   const user = users.find((u) => u.id === userId);
-  userName = 'Adrien';
   if (!user) {
     return res.status(404).json({ message: `L'utilisateur ${userId} n'existe pas.` });
   }
@@ -37,6 +70,7 @@ app.post('/users/:id/accounts/debit', (req, res) => {
   if (amount <= account.balance) {
     // Retirer le montant du solde du compte
     account.balance -= amount;
+    saveAccountData(); // Save updated account data to JSON file
     return res.json({ message: `Débit de ${amount} effectué pour l'utilisateur ${userId}. Le solde est maintenant de ${account.balance}.` });
   } else {
     return res.status(400).json({ message: `Le montant demandé est supérieur au solde du compte.` });
@@ -59,17 +93,17 @@ app.post('/users/:id/accounts/credit', (req, res) => {
   if (!account) {
     return res.status(404).json({ message: `Aucun compte trouvé pour l'utilisateur ${userId}.` });
   }
-  console.log(account.amount);
+
   // Verifier que le montant à créditer est inférieur ou égal à 1000
   if (amount <= 1000) {
     // Ajouter le montant au solde du compte
     account.balance += amount;
-    
-    return res.json({ message: `Crédit de ${amount} effectué pour l'utilisateur ${userId}.` });
+    saveAccountData(); // Save updated account data to JSON file
+    return res.json({ message: `Crédit de ${amount} effectué pour l'utilisateur ${userId}. Le solde est maintenant de ${account.balance}.` });
   } else {
     // Créditer le compte avec une partie du montant pour ne pas dépasser 1000
     account.balance += 1000 - account.balance;
-    // Retourner un message d'erreur et le montant crédité et le montant restant
+    saveAccountData(); // Save updated account data to JSON file
     return res.status(400).json({ message: `Le montant demandé est supérieur à 1000. Seul ${1000 - account.balance} a été crédité.` });
   }
 });
